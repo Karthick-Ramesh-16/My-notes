2e09a28b587e1c9ce0c0a94120bff02a14c2a363
karthick-ramesh-16 <karthick.ramesh@freshworks.com>
2022-10-31 02:01:14 +0530

    Change JavaScript to JQuery

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-document.getElementById("note-title").value = sessionStorage.getItem("title");
-document.getElementById("note-content").value = sessionStorage.getItem("content");
+$(function() {
+    $("#note-title").val(sessionStorage.getItem("title"));
+    $("#note-content").text(sessionStorage.getItem("content"));
 
-
-function saveNote() {
-    sessionStorage.setItem("title", document.getElementById("note-title").value);
-    sessionStorage.setItem("content", document.getElementById("note-content").value);
-    console.log(document.getElementById("note-title").value);
-    savedMessage();
-}
-
-
-function savedMessage() {
-    let note_saved = document.getElementById("note-saved");
-    note_saved.style.display = "block";
-    note_saved.style.display = "flex";
-    note_saved.style.justifyContent = "space-between";
-}
+    $("#note-save-btn").click(function() {
+        sessionStorage.setItem("title", $("#note-title").val());
+        sessionStorage.setItem("content", $("#note-content").val());
+        $("#note-saved").css("display", "flex");
+        $("#note-saved").css("justifyContent", "space-between");
+    });
+});
